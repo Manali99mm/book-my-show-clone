b771a95f5fc166a196c2359557a1106158443e12
Manali Mathur <a.manalimathur@gmail.com>
2021-07-28 00:02:36 +0530

    initial commit

---
+++ planning.js
@@ -0,0 +1,20 @@
+// Book my show planning
+
+// -> we need a reusable carousel
+//      -> Hero carousel
+//      -> Common carousel
+//      -> cast and creq small carousel
+
+
+// Pages:-
+// -> Home Page
+// -> Movie Page
+// -> Categories page
+// -> Auth Page
+// -> Razorpay payment gateway
+
+// Plan - How are we building it???
+// layouts
+// Routes
+// Pages
+// Components
