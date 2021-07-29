c8a0842124dfe585e26713ae9f8d1fde8d59f548
Manali Mathur <a.manalimathur@gmail.com>
2021-07-29 18:55:56 +0530

    add movie layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 // HOC
 import DefaultHOC from "./HOC/Default.HOC";
+import MovieHOC from "./HOC/Movie.HOC";
 
 // Pages
 import HomePage from "./pages/Home.page";
+import Movie from "./pages/Movie.page";
 
 function App() {
   return (
     <>
       <DefaultHOC path="/" exact component={HomePage} />
+      <MovieHOC path="/movie/:id" exact component={Movie} />
     </>
   );
 }
