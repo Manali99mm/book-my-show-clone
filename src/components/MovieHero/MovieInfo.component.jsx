b610e29fe0f7104f2753cd4850758716781ef727
Manali Mathur <a.manalimathur@gmail.com>
2021-08-02 23:53:37 +0530

    razorpay payment gateway integration

--- src/components/MovieHero/MovieInfo.component.jsx
+++ src/components/MovieHero/MovieInfo.component.jsx
@@ -1,15 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+
+// Components
+import PaymentModal from '../PaymentModal/Payment.component';
 
 // context
 import { MovieContext } from '../../context/movie.context';
 
 const MovieInfo = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [price, setPrice] = useState(0);
+    const [paymentTitle, setPaymentTitle] = useState("");
+    const [paymentDescription, setPaymentDescription] = useState("");
+
     const { movie } = useContext(MovieContext);
 
     const genres = movie.genres?.map(({ name }) => name).join(", ")
 
+    const rentMovies = () => {
+        setIsOpen(true);
+        setPrice(149);
+        setPaymentTitle("Rent")
+        setPaymentDescription("You can rent this movie for 30 days but will have 2 days to watch it once you start playback (available for download)")
+    }
+
+    const buyMovies = () => {
+        setIsOpen(true);
+        setPrice(599);
+        setPaymentTitle("Buy")
+        setPaymentDescription("Watch the movie any time after purchasing it (available for download)")
+    }
+
     return (
         <>
+            <PaymentModal title={paymentTitle} description={paymentDescription} isOpen={isOpen} setIsOpen={setIsOpen} price={price} />
             <div className="flex flex-col gap-3 px-2 lg:gap-6">
                 <div className="flex items-center gap-3 md:px-4">
                     <div className="h-6 w-30">
@@ -24,8 +47,8 @@ const MovieInfo = () => {
                         <h4>{parseInt(movie.runtime / 60)}h {parseInt(movie.runtime % 60)}m &bull; {genres} &bull; {movie.adult ? "18+" : "13+"} &bull; {movie.release_date}</h4>
                     </div>
                     <div className="flex text-white items-center gap-3 md:px-4 md:w-screen lg:w-full">
-                        <button className="bg-navBtn-300 py-3 w-full font-semibold rounded-lg">Rent ₹149</button>
-                        <button className="bg-navBtn-300 py-3 w-full font-semibold rounded-lg">Buy ₹689</button>
+                        <button onClick={rentMovies} className="bg-navBtn-300 py-3 w-full font-semibold rounded-lg">Rent ₹149</button>
+                        <button onClick={buyMovies} className="bg-navBtn-300 py-3 w-full font-semibold rounded-lg">Buy ₹599</button>
                     </div>
                 </div>
             </div>
